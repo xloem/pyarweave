80c12b6a68a70a8d4724e88b73068732fb516044
John Doe <johndoe@example.com>
2022-05-19 05:07:38 -0400

    relicense py files in test

--- test/simple_http_server.py
+++ test/simple_http_server.py
@@ -1,3 +1,17 @@
+# This file is part of PyArweave.
+# 
+# PyArweave is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 2 of the License, or (at your option) any later
+# version.
+# 
+# PyArweave is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# PyArweave. If not, see <https://www.gnu.org/licenses/>.
+
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 from io import BytesIO
