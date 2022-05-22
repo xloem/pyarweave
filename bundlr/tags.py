10816ef5f839a0ab73a9be7b50bee791ccd7ee2a
John Doe <johndoe@example.com>
2022-05-22 05:43:30 -0400

    bundlr.tags : use bytes

--- bundlr/tags.py
+++ bundlr/tags.py
@@ -7,22 +7,30 @@ schema = fastavro.parse_schema({
     "type": "record",
     "name": "Tag",
     "fields": [
-      { "name": "name", "type": "string" },
-      { "name": "value", "type": "string" }
+      { "name": "name", "type": "bytes" },
+      { "name": "value", "type": "bytes" }
     ]
   }  
 })
 
-def serialize_buffer(tags_dict):
+def serialize_buffer(tags_obj):
     output = io.BytesIO()
-    tag_records = [{'name':name,'value':value} for name, value in tags_dict.items()]
+    if isinstance(tags_obj, dict):
+        tags_obj = tags_obj.items()
+    tag_records = [
+        {
+            'name': name if isinstance(name,(bytes,bytearray)) else str(name).encode(),
+            'value': value if isinstance(value,(bytes,bytearray)) else str(value).encode()
+        }
+        for name, value in tags_obj
+    ]
     fastavro.schemaless_writer(output, schema, tag_records)
     return output.getbuffer()
 
 def deserialize_buffer(data):
     fo = io.BytesIO(data)
     records = fastavro.schemaless_reader(fo, schema)
-    return {
-        record['name']: record['value']
+    return [
+        (record['name'], record['value'])
         for record in records
-    }
+    ]
