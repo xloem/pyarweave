2a10d9bb7d5784e721660c49d75bf13b2e0c74a8
John Doe <johndoe@example.com>
2022-05-24 12:55:09 -0400

    work toward block serialisation

---
+++ ar/block.py
@@ -0,0 +1,183 @@
+import fractions
+import io
+from ar.utils import (
+    arbinenc, arintenc, arbindec, arintdec, erlintenc, erlintdec, b64enc_if_not_str, b64enc, b64dec
+)
+
+class Block:
+    def __init__(self, indep_hash, prev_block, timestamp, nonce,
+                 height, diff, cumulative_diff, last_retarget,
+                 hash, block_size, weave_size, reward_addr,
+                 tx_root, wallet_list, hash_list_merkle, reward_pool,
+                 packing_threshold, strict_chunk_threshold,
+                 usd_to_ar_rate, scheduled_usd_to_ar_rate,
+                 poa_option, poa_chunk, poa_tx_path, poa_data_path,
+                 tags = [], txs = []):
+        self.indep_hash = b64enc_if_not_str(indep_hash)
+        self.prev_block = b64enc_if_not_str(prev_block)
+        self.timestamp = timestamp
+        self.nonce = b64enc_if_not_str(nonce)
+        self.height = height
+        self.diff = diff
+        self.cumulative_diff = cumulative_diff
+        self.last_retarget = last_retarget
+        self.hash = b64enc_if_not_str(hash)
+        self.block_size = block_size
+        self.weave_size = weave_size
+        self.reward_addr = b64enc_if_not_str(reward_addr)
+        self.tx_root = b64enc_if_not_str(tx_root)
+        self.wallet_list = b64enc_if_not_str(wallet_list)
+        self.hash_list_merkle = b64enc_if_not_str(hash_list_merkle)
+        self.reward_pool = reward_pool
+        self.packing_threshold = packing_threshold
+        self.strict_chunk_threshold = strict_chunk_threshold
+
+        if type(usd_to_ar_rate) is not tuple:
+            usd_to_ar_rate = fractions.Fraction(usd_to_ar_rate)
+            self.usd_to_ar_rate_raw = (usd_to_ar_rate.numerator, usd_to_ar_rate.denominator)
+        else:
+            self.usd_to_ar_rate_raw = usd_to_ar_rate
+
+        if type(scheduled_usd_to_ar_rate) is not tuple:
+            scheduled_usd_to_ar_rate = fractions.Fraction(scheduled_usd_to_ar_rate)
+            self.scheduled_usd_to_ar_rate_raw = (
+                scheduled_usd_to_ar_rate.numerator, scheduled_usd_to_ar_rate.denominator
+            )
+        else:
+            self.scheduled_usd_to_ar_rate_raw = scheduled_usd_to_ar_rate
+
+        self.poa_option = poa_option
+        self.poa_chunk = b64enc_if_not_str(poa_chunk)
+        self.poa_tx_path = b64enc_if_not_str(poa_tx_path)
+        self.poa_data_path = b64enc_if_not_str(poa_data_path)
+
+        self.tags = tags
+        if len(txs) and isinstance(txs[0], (bytes, bytearray)):
+            self.txs = [b64enc_if_not_str(tx) for tx in txs[::-1]]
+        else:
+            self.txs = txs
+
+    def __getattr__(self, attr):
+        if attr.endswith('_raw'):
+            return b64dec(getattr(self, attr[:-4]))
+
+    @property
+    def usd_to_ar_rate(self):
+        try:
+            return fractions.Fraction(*self.usd_to_ar_rate_raw)
+        except ZeroDivisionError:
+            return None
+
+    @property
+    def scheduled_usd_to_ar_rate(self):
+        try:
+            return fractions.Fraction(*self.sceduled_usd_to_ar_rate_raw)
+        except ZeroDivisionError:
+            return None
+
+    @classmethod
+    def frombytes(cls, bytes):
+        stream = io.BytesIO(bytes)
+        return cls.fromstream(stream)
+            
+    @classmethod
+    def fromstream(cls, stream):
+        indep_hash_raw               = stream.read(48)
+        prev_block_raw               = arbindec(stream,  8)
+        timestamp                    = arintdec(stream,  8)
+        nonce_raw                    = arbindec(stream, 16)
+        height                       = arintdec(stream,  8)
+        diff                         = arintdec(stream, 16)
+        cumulative_diff              = arintdec(stream, 16)
+        last_retarget                = arintdec(stream,  8)
+        hash_raw                     = arbindec(stream,  8)
+        block_size                   = arintdec(stream, 16)
+        weave_size                   = arintdec(stream, 16)
+        reward_addr_raw              = arbindec(stream,  8)
+        tx_root_raw                  = arbindec(stream,  8)
+        wallet_list_raw              = arbindec(stream,  8)
+        hash_list_merkle_raw         = arbindec(stream,  8)
+        reward_pool                  = arintdec(stream,  8)
+        packing_2_5_threshold        = arintdec(stream,  8)
+        strict_data_split_threshold  = arintdec(stream,  8)
+        usd_to_ar_rate_raw           =(arintdec(stream,  8),
+                                       arintdec(stream,  8))
+        scheduled_usd_to_ar_rate_raw =(arintdec(stream,  8),
+                                       arintdec(stream,  8))
+        poa_option                   = arintdec(stream,  8)
+        poa_chunk_raw                = arbindec(stream, 24)
+        poa_tx_path_raw              = arbindec(stream, 24)
+        poa_data_path_raw            = arbindec(stream, 24)
+
+        tags_count = erlintdec(stream, 16)
+        tags       = [arbindec(stream, 16) for idx in range(tags_count)]
+
+        # either 32-byte txids or complete txs
+        txs_count = erlintdec(stream, 16)
+        txs_raw = [arbindec(stream, 24) for idx in range(txs_count)]
+        txs = [b64enc(tx) for tx in txs_raw[::-1]]
+
+        return cls(indep_hash = indep_hash_raw, prev_block = prev_block_raw,
+                   timestamp = timestamp, nonce = nonce_raw, height = height,
+                   diff = diff, cumulative_diff = cumulative_diff,
+                   last_retarget = last_retarget, hash = hash_raw,
+                   block_size = block_size, weave_size = weave_size,
+                   reward_addr = reward_addr_raw, tx_root = tx_root_raw,
+                   wallet_list = wallet_list_raw,
+                   hash_list_merkle = hash_list_merkle_raw,
+                   reward_pool = reward_pool,
+                   packing_threshold = packing_2_5_threshold,
+                   strict_chunk_threshold = strict_data_split_threshold,
+                   usd_to_ar_rate = usd_to_ar_rate_raw,
+                   scheduled_usd_to_ar_rate = scheduled_usd_to_ar_rate_raw,
+                   poa_option = poa_option, poa_chunk = poa_chunk_raw,
+                   poa_tx_path = poa_tx_path_raw,
+                   poa_data_path = poa_data_path_raw,
+                   tags = tags, txs = txs)
+
+    def tobytes(self):
+        stream = io.BytesIO()
+        stream.write(self.indep_hash_raw)
+        stream.write(arbinenc(self.prev_block_raw,                  8))
+        stream.write(arintenc(self.timestamp,                       8))
+        stream.write(arbinenc(self.nonce_raw,                      16))
+        stream.write(arintenc(self.height,                          8))
+        stream.write(arintenc(self.diff,                           16))
+        stream.write(arintenc(self.cumulative_diff,                16))
+        stream.write(arintenc(self.last_retarget,                   8))
+        stream.write(arbinenc(self.hash_raw,                        8))
+        stream.write(arintenc(self.block_size,                     16))
+        stream.write(arintenc(self.weave_size,                     16))
+        stream.write(arbinenc(self.reward_addr_raw,                 8))
+        stream.write(arbinenc(self.tx_root_raw,                     8))
+        stream.write(arbinenc(self.wallet_list_raw,                 8))
+        stream.write(arbinenc(self.hash_list_merkle_raw,            8))
+        stream.write(arintenc(self.reward_pool,                     8))
+        stream.write(arintenc(self.packing_threshold,               8))
+        stream.write(arintenc(self.strict_chunk_threshold,          8))
+        stream.write(arintenc(self.usd_to_ar_rate_raw[0],           8))
+        stream.write(arintenc(self.usd_to_ar_rate_raw[1],           8))
+        stream.write(arintenc(self.scheduled_usd_to_ar_rate_raw[0], 8))
+        stream.write(arintenc(self.scheduled_usd_to_ar_rate_raw[1], 8))
+        stream.write(arintenc(self.poa_option,                      8))
+        stream.write(arbinenc(self.poa_chunk_raw,                  24))
+        stream.write(arbinenc(self.poa_tx_path_raw,                24))
+        stream.write(arbinenc(self.poa_data_path_raw,              24))
+        stream.write(erlintenc(len(self.tags), 16))
+        import pdb; pdb.set_trace()
+        # encoding issues appeart to start on or after here
+        for tag in self.tags:
+            stream.write(arbinenc(tag,                             16))
+        stream.write(erlintenc(len(self.txs), 16))
+        for tx in self.txs:
+            stream.write(arbinenc(b64dec(tx),                      24))
+        return stream.getvalue()
+
+if __name__ == '__main__':
+    import ar
+    peer = ar.Peer()
+    height = 940318#peer.height()
+    blockbytes = peer.block2(height)
+    block = Block.frombytes(blockbytes)
+    assert block.tobytes() == blockbytes
+    
