62d471e7337d1b7e39cefcc1ee2a02eecffa0f3c
John Doe <johndoe@example.com>
2022-06-11 10:23:23 -0400

    stubbed manifest class

---
+++ ar/manifest.py
@@ -0,0 +1,29 @@
+class Manifest:
+    def __init__(self, path_txids, index = None, version = '0.1.0'):
+        self.path_txids = path_txids
+        self.index = index
+        self.version = version
+    @classmethod
+    def fromjson(cls, json):
+        return cls(
+            path_txids = { key: value['id'] for key, value in json['paths'].items() },
+            index = json['index']['path'] if 'index' in json else None,
+            version = json['version']
+        )
+    def tojson(self):
+        return dict(
+            manifest = 'arweave/paths',
+            version = self.version,
+            **(
+                dict(
+                    index = dict(
+                        path = self.index
+                    )
+                ) if self.index is not None
+                else {}
+            ),
+            paths = {
+                path: dict(id=txid)
+                for path, txid in self.path_txids.items()
+            }
+        )
