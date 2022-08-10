b59812ca61a99afbc045e71c3a69788b0d761c3b
John Doe <john.doe@example.com>
2022-08-10 13:45:30 -0400

    5.00

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='PyArweave',
-  version='0.4.16',
+  version='0.5.00',
   description='Tiny Arweave Library',
   long_description=open('README.md').read(),
   long_description_content_type='text/markdown',
