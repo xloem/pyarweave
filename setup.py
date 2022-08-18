7e9a51287cae8889322cb24016ccb39ab96fda2d
Karl Semich <0xloem@gmail.com>
2022-08-18 06:55:15 -0400

    0.5.9

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='PyArweave',
-  version='0.5.8',
+  version='0.5.9',
   description='Tiny Arweave Library',
   long_description=open('README.md').read(),
   long_description_content_type='text/markdown',
