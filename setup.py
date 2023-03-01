6d0729ccbbb0c4379a1d54aebeecdd4dccfe8fa8
xloem <0xloem@gmail.com>
2023-02-28 21:01:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
   name='PyArweave',
   version='0.4.15',
   description='Tiny Arweave Library',
-  author = 'Mike Hibbert',
+  author = 'xloem',
   long_description=open('README.md').read(),
   long_description_content_type='text/markdown',
   url='https://github.com/xloem/pyarweave',
