3331d5a39cff441088f28cbf39a3f717e47003af
John Doe <john.doe@example.com>
2023-05-30 17:53:12 -0400

    update ans104 signer types

--- ar/utils/ans104_signers.py
+++ ar/utils/ans104_signers.py
@@ -25,10 +25,11 @@ class Signer:
     def public_key(cls, raw_owner):
         raise NotImplementedError(cls.__name__ + '.public_key')
 
-class Rsa4096Pss(Signer): # arweave-style
+class Arweave(Signer): # rsa4096pss
     type = 1
     owner_length = 512
     signature_length = 512
+    name = 'arweave'
 
     @classmethod
     def sign(cls, private_key, databytes):
@@ -57,20 +58,47 @@ class Rsa4096Pss(Signer): # arweave-style
     def public_key(raw_owner, consistency_check = True):
         return RSA.construct((bytes_to_long(raw_owner), 65537), consistency_check=consistency_check)
 
-class Curve25519(Signer):
+Rsa4096Pss = Arweave
+
+class Ed25519(Signer):
     type = 2
     owner_length = 32
     signature_length = 64
+    name = 'ed25519'
 
-class Secp256k1(Signer): # ethereum-style
+Curve25519 = Ed25519
+
+class Ethereum(Signer):
     type = 3
     owner_length = 65
     signature_length = 65
+    name = 'ethereum'
+
+Secp256k1 = Ethereum
 
 class Solana(Signer):
     type = 4
     owner_length = 32
     signature_length = 64
+    name = 'solana'
+
+class InjectedAptos(Signer):
+    type = 5
+    owner_length = 32
+    signature_length = 64
+    name = 'injectedAptos'
+
+class MultiAptos(Signer):
+    type = 6
+    owner_length = 32 * 32 + 1
+    signature_length = 64 * 32 + 4
+    name = 'multiAptos'
+
+class TypedEthereum(Signer):
+    type = 7
+    owner_length = 42
+    signature_length = 65
+    name = 'typedEthereum'
 
 BY_TYPE = {
     keycfg.type: keycfg
