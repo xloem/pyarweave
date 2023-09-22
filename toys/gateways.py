84e638efba1e5ce485aa112640a33b3f6a4d5e15
John Doe <johndoe@example.com>
2023-09-22 08:18:11 -0400

    gateways can measure speed of fetching something

--- toys/gateways.py
+++ toys/gateways.py
@@ -1,14 +1,51 @@
+#!/usr/bin/env python3
 from ar import DEFAULT_GATEWAY_ADDRESS_REGISTRY_CACHE
 import requests
+import sys, time
+from pqdm.threads import pqdm # maybe joblib Parallel verbose=True instead
 
-cache = requests.get(DEFAULT_GATEWAY_ADDRESS_REGISTRY_CACHE).json()
-for id, entry in cache['gateways'].items():
+if len(sys.argv) > 1:
+    fetchid = sys.argv[1]
+#    fastest_time = float('inf')
+#    fastest_url = None
+else:
+    fetchid = None
+
+def measure_entry(entry):
+    start = time.time()
+    url = entry['url']
+    try:
+        response = requests.get(f'{url}/{fetchid}', timeout=15)
+        response.raise_for_status()
+    except:
+        pass
+    else:
+        entry['duration'] = time.time() - start
+
+def process_entry(id, entry):
+    entry['id'] = id
     settings = entry['settings']
-    if entry['status'] != 'joined':
-        continue
     protoport = [settings['protocol'], settings['port']]
     if protoport in [['http',80],['https',443]]:
         url = '{protocol}://{fqdn}'.format(**settings)
     else:
         url = '{protocol}://{fqdn}:{port}'.format(**settings)
-    print(url)
+    entry['url'] = url
+    if fetchid is not None and entry['status'] == 'joined':
+        measure_entry(entry)
+    return entry
+
+cache = requests.get(DEFAULT_GATEWAY_ADDRESS_REGISTRY_CACHE).json()
+gws = pqdm(
+    cache['gateways'].items(), process_entry, argument_type='args',
+    n_jobs=32,
+    unit='gw', leave=False, desc='measuring',
+)
+if fetchid is not None:
+    gws = [[gw['duration'], gw['url']] for gw in gws if 'duration' in gw]
+    gws.sort()
+    for time, url in gws[:4]:
+        print(url)
+else:
+    for gw in gws:
+        print(gw['url'])
