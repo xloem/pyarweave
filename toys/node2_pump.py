3b3acf72c0c04a95f1e2f1585cf1c95a6cb9e775
John Doe <johndoe@example.com>
2024-01-05 12:31:04 -0800

    -

--- toys/node2_pump.py
+++ toys/node2_pump.py
@@ -2,33 +2,206 @@ import concurrent.futures
 import threading, queue
 import time
 
+import ar
+import tqdm
+
 # note: encoding may be cpu-bound
 
-class InsertableRateQueueIterable:
-    def __init__(self, at_once=10, period_secs=1):
+class DeferedProxiedFuture(concurrent.futures.Future):
+    def proxy(self, fut):
+        fut.add_done_callback(self._proxy_result)
+    def _proxy_result(self, fut):
+        try:
+            self.set_result(fut.result())
+        except concurrent.futures.CancelledError:
+            self.cancel()
+        except Exception as e:
+            self.set_exception(e)
+
+class Bisect:
+    def __init__(self, node, data, wallet, TOTAL_COUNT):
+        self.node = node
+        self.data = data
+        self.wallet = wallet
+        self.lock = threading.Lock()
+        self.TOTAL_COUNT = TOTAL_COUNT
+    def encode(self):
+        self.dataitems = []
+        for idx in tqdm.tqdm(range(len(self.data)),total=len(self.data),desc='Encoding',leave=False,unit='di'):
+            d = self.data[idx]
+            di = ar.DataItem(data=d)
+            di.sign(self.wallet.rsa)
+            bytes = di.tobytes()   
+            self.dataitems.append(bytes)
+    def send(self, item):
+        result = self.node.send_tx(item)
+        with self.lock:
+            self.pbar.update(100000)
+    def go(self):
+        self.encode()
+        self.pbar = tqdm.tqdm(total=100000*len(self.data), unit='B', unit_scale=True, smoothing=0)
+        with self.pbar, concurrent.futures.ThreadPoolExecutor() as pool:
+            mark = time.time() - 1
+            for chunk_offset in range(0, self.TOTAL_COUNT, 10):
+                results = pool.map(self.send, self.dataitems[chunk_offset:chunk_offset+10])
+                results = list(results)
+                now = time.time()
+                mark += 1
+                if now < mark:
+                    self.pbar.display(f'sleeping for {mark - now}')
+                    time.sleep(mark - now)
+
+class Bisect2:
+    def __init__(self, node, wallet, at_once=10, period_secs=1):
+        self.node = node
+        self.wallet = wallet
         self.at_once = at_once
         self.period_secs = period_secs
-        self.queue = queue.Queue()
-        self.insertions = queue.Queue()
-    def add(self, item):
-        self.queue.put(item)
-    def insert(self, item):
-        self.inserts.put(item)
-    def __iter__(self):
-        mark = time.time() - self.period_secs
-        ct = 0
-        while True:
-            for ct in range(self.at_once):
-                if self.insertions.qsize():
-                    yield self.insertions.get()
-                elif self.queue.qsize():
-                    yield self.queue.get()
-                else:
-                    return
-            now = time.time()
-            mark += self.period_secs
-            if now < mark:
-                time.sleep(mark - now)
+        #self.dataitems = []
+        self.queue_bg = queue.Queue()
+        self.queue_fg = queue.Queue()
+    def _encode(self, d):
+        di = ar.DataItem(data=d)
+        di.sign(self.wallet.rsa)
+        return di.tobytes()   
+    def feed(self, data):
+        for idx in tqdm.tqdm(range(len(data)),total=len(data),desc='Encoding',leave=False,unit='di'):
+            self.queue_bg.put(self._encode(data[idx]))
+    #def immed(self, data):
+    def go(self):
+        th = threading.Thread(target=self._go)
+        th.start()
+        return th.join()
+    def _go(self):
+        self.pbar = tqdm.tqdm(total=100000*self.queue_bg.qsize(), unit='B', unit_scale=True, smoothing=0)
+        with self.pbar, concurrent.futures.ThreadPoolExecutor() as pool:
+            mark = time.time() - self.period_secs
+            futs = []
+            tx_count = -self.at_once
+            while self.queue_bg.qsize() or self.queue_fg.qsize():
+                di = self.queue_bg.get()
+                while True:
+                    now = time.time()
+                    schedule = tx_count * self.period_secs / self.at_once + mark
+                    if schedule <= now:
+                        futs.append(pool.submit(self.node.send_tx, di))
+                        tx_count += 1
+                        break
+                    done, futs = concurrent.futures.wait(
+                        futs,
+                        timeout=schedule-now,
+                        return_when=concurrent.futures.FIRST_COMPLETED
+                    )
+                    self.pbar.update(100000*len(done))
+                    futs = list(futs)
+            while len(futs):
+                done, futs = concurrent.futures.wait(
+                    futs,
+                    timeout=None,
+                    return_when=concurrent.futures.FIRST_COMPLETED
+                )
+                self.pbar.update(100000*len(done))
+
+class Bisect3:
+    def __init__(self, node, wallet, at_once=10, period_secs=1):
+        self.node = node
+        self.wallet = wallet
+        self.at_once = at_once
+        self.period_secs = period_secs
+        self.queue_bg_in = queue.Queue()
+        self.queue_fg = queue.Queue()
+        self.queue_bg_out = queue.Queue()
+        self.thread = None
+    def _encode(self, d):
+        di = ar.DataItem(data=d)
+        di.sign(self.wallet.rsa)
+        return di.tobytes()   
+    def feed(self, data):
+        for idx in tqdm.tqdm(range(len(data)),total=len(data),desc='Encoding',leave=False,unit='di'):
+            fut = DeferedProxiedFuture()
+            self.queue_bg_in.put([self._encode(data[idx]), fut])
+            self.queue_bg_out.put(fut)
+            if self.thread is None:
+                self.thread = threading.Thread(target=self._go)
+                self.thread.start()
+    def fetch(self, ct):
+        return (self.queue_bg_out.get().result() for x in range(ct))
+    def immed(self, data):
+        fut = DeferedProxiedFuture()
+        self.queue_fg.put([self._encode(data[idx]), fut])
+        return fut.result()
+    def _go(self):
+        with concurrent.futures.ThreadPoolExecutor() as pool:
+            mark = time.time()
+            next_mark = mark + self.period_secs
+            sys_futs = set()
+            tx_count = 0
+            while self.queue_bg_in.qsize() or self.queue_fg.qsize():
+                try:
+                    di, user_fut = self.queue_fg.get_nowait()
+                except queue.Empty:
+                    di, user_fut = self.queue_bg_in.get_nowait()
+                while True:
+                    now = time.time()
+                    if now > next_mark:
+                        tx_count = 0
+                        next_mark = now + self.period_secs
+                    if tx_count < self.at_once:
+                        sys_fut = pool.submit(self.node.send_tx, di)
+                        user_fut.proxy(sys_fut)
+                        sys_futs.add(sys_fut)
+                        tx_count += 1
+                        break
+                    done, sys_futs = concurrent.futures.wait(
+                        sys_futs,
+                        timeout=next_mark-now,
+                        return_when=concurrent.futures.FIRST_COMPLETED
+                    )
+                    #self.pbar.update(100000*len(done))
+            while len(sys_futs):
+                done, sys_futs = concurrent.futures.wait(
+                    sys_futs,
+                    timeout=None,
+                    return_when=concurrent.futures.FIRST_COMPLETED
+                )
+                #self.pbar.update(100000*len(done))
+        self.thread = None
+
+#class Pump2:
+#    def __init__(self, at_once=10, period_secs=1):
+#        self.at_once = at_once
+#        self.period_secs = period_secs
+#    def submit(self, item):
+#        self.queue.put(item)
+#    def immediate(self, item):
+#        self.inserts.put(item)
+
+#class InsertableRateQueueIterable:
+#    def __init__(self, at_once=10, period_secs=1):
+#        self.at_once = at_once
+#        self.period_secs = period_secs
+#        self.queue = queue.Queue()
+#        self.insertions = queue.Queue()
+#    def add(self, item):
+#        self.queue.put(item)
+#    def insert(self, item):
+#        self.inserts.put(item)
+#    def __iter__(self):
+#        import pdb; pdb.set_trace()
+#        mark = time.time() - self.period_secs
+#        ct = 0
+#        while True:
+#            for ct in range(self.at_once):
+#                if self.insertions.qsize():
+#                    yield self.insertions.get()
+#                elif self.queue.qsize():
+#                    yield self.queue.get()
+#                else:
+#                    return
+#            now = time.time()
+#            mark += self.period_secs
+#            if now < mark:
+#                time.sleep(mark - now)
 
 class Pump(threading.Thread):
     def __init__(self, node, at_once=10, period_secs=1):
@@ -38,6 +211,11 @@ class Pump(threading.Thread):
         self.at_once = at_once
         self.period_secs = period_secs
         super().__init__()
+    #def __enter__(self):
+    #    self._executor = ThreadPoolExecutor(max_workers = self.at_once)
+    #    self._executor.__enter__()
+    #def __exit__(self, e1, e2, e3):
+    #    return self._executor.__exit__(e1, e2, e3)
     def enqueue(self, dataitem):
         fut = concurrent.futures.Future()
         self.queue.put([dataitem, fut])
