900f6707cd8f7d3ba7d941a3808b2451ebe123f5
John Doe <johndoe@example.com>
2024-02-07 14:59:44 -0800

    -

--- toys/ditem_down_simple.bash
+++ toys/ditem_down_simple.bash
@@ -4,11 +4,11 @@ GW=https://arweave.net/
 DATA=$(mktemp)
 cat > "$DATA"
 SIZE=$(head -n1 < "$DATA" | jq -r .size)
-DEPTH=$(head -n1 < "$DATA" | jq -r .depth)
+DEPTH=$(head -n1 < "$DATA" | jq -r '.depth // 1')
 for ((d=DEPTH; d>0; d--))
 do
     mv "$DATA" "$DATA".old
-    jq -r "\"$GW\" + .id" < "$DATA".old | parallel -k -j0 curl -sL | pv --name "depth=$d" -s "$SIZE" > "$DATA"
+    jq -r "\"$GW\" + .id" < "$DATA".old | parallel -k -j128 -N16 curl -sL | pv -pteab --name "depth=$d" -s "$SIZE" > "$DATA"
     rm "$DATA".old
 done
 cat "$DATA"
