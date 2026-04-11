83ca976725d02b67e7041a5633e076c681a935b6
Your Name <you@example.com>
2026-04-11 10:17:45 -0400

    ao constant

--- ao/__init__.py
+++ ao/__init__.py
@@ -1,15 +1,20 @@
-DEFAULT_MU_HOST = 'https://mu.ao-testnet.xyz'
-DEFAULT_CU_HOST = 'https://cu.ao-testnet.xyz'
+#DEFAULT_MU_HOST = 'https://mu.ao-testnet.xyz'
+#DEFAULT_CU_HOST = 'https://cu.ao-testnet.xyz'
+DEFAULT_CU_HOST = 'https://cu.ardrive.io'
 
 # made of DataItems with tags Data-Protocol and Type=Process or Type=Message
 # ao/servers/su/src/domain/core/flows.rs
 
 from .cu import ComputeUnit
-from .mu import MessengerUnit
+#from .mu import MessengerUnit
 from .su import SchedulerUnit
 
 cu = ComputeUnit
-mu = MessengerUnit
+#mu = MessengerUnit
 su = SchedulerUnit
 
+# ar-io-sdk/src/constants.ts
+
+AR_IO_DEVNET_PROCESS_ID = 'GaQrvEMKBpkjofgnBi_B3IgIDmY_XYelVLB6GcRGrHc'
 AR_IO_TESTNET_PROCESS_ID = 'agYcCFJtrMG6cqMuZfskIkFTGvUPddICmtQSBIoPdiA'
+AR_IO_MAINNET_PROCESS_ID = 'qNvAoz0TgcH7DMg8BCVn8jF32QH5L6T29VjHxhHqqGE'
